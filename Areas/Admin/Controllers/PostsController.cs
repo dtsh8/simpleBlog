479661ef0734670828330da0b7c903ddb33420c1
tashido <shirapovich@yandex.ru>
2014-05-27 23:55:18 +0400

    Created New anв Edit actions for Posts
    Created New anв Edit actions for Posts

--- Areas/Admin/Controllers/PostsController.cs
+++ Areas/Admin/Controllers/PostsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using NHibernate.Linq;
 using SimpleBlog.Areas.Admin.ViewModels;
@@ -17,7 +18,7 @@ namespace SimpleBlog.Areas.Admin.Controllers
         {
             var totalPostCount = Database.Session.Query<Post>().Count();
             var currentPostPage = Database.Session.Query<Post>()
-                .OrderByDescending(c => c.CreatedAt)
+                .OrderBy(c => c.CreatedAt)
                 .Skip((page - 1)*PostsPerPage)
                 .Take(PostsPerPage)
                 .ToList();
@@ -26,6 +27,71 @@ namespace SimpleBlog.Areas.Admin.Controllers
             {
                 Posts = new PageData<Post>(currentPostPage, totalPostCount, page, PostsPerPage)
             });
+        }
+
+        public ActionResult New()
+        {
+            return View("Form", new PostsForm
+            {
+                IsNew = true
+            });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var post = Database.Session.Load<Post>(id);
+
+            if (post == null)
+                return HttpNotFound();
+
+            return View("FOrm", new PostsForm
+            {
+                IsNew = false,
+                PostId = id,
+                Content = post.Content,
+                Slug = post.Slug,
+                Title = post.Title
+            });
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Form(PostsForm form)
+        {
+            form.IsNew = form.PostId == null;
+
+            if (!ModelState.IsValid)
+            {
+                return View(form);
+            }
+
+            Post post;
+
+            if (form.IsNew)
+            {
+                post = new Post
+                {
+                    CreatedAt = DateTime.UtcNow,
+                    User = Auth.User
+                };
+            }
+            else
+            {
+                post = Database.Session.Load<Post>(form.PostId);
+
+                if (post == null)
+                    return HttpNotFound();
+
+                post.UpdatedAt = DateTime.UtcNow;
+            }
+
+            post.Title = form.Title;
+            post.Slug = form.Slug;
+            post.Content = form.Content;
+
+            Database.Session.SaveOrUpdate(post);
+
+            return RedirectToAction("Index");
+
         }
     }
 
